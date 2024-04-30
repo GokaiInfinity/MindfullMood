3583cc87599b1c2f00e201928697c6d420b09c07
GokaiInfinity <patriciofilbert@gmail.com>
2024-04-30 18:59:43 +0700

    Bootstrap
    test

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -14,7 +14,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=League+Spartan:wght@100..900&display=swap" rel="stylesheet">
-    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <!-- Style -->
     <style>
     *{
@@ -28,7 +28,7 @@
       padding: 10px 20px; /* Adjust padding as needed */
       transition: all 0.3s ease; /* Smooth transition */
     }
-    
+
     .square-btn:hover {
       background-color: black;
       color: white;
