811a547a9b27dc490fbbf8ea5a9b526d44813b67
JDTOmoi <jasondavetahir@gmail.com>
2024-05-04 16:30:43 +0700

    footer updates

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,24 +1,25 @@
-<div class="container-fluid" style="background-color:#15184D" >
-    <footer>
-        
-    <div class="container pt-2 pb-5">
-      <div class="row row-cols-1 row-cols-md-3 text-white pb-5">
-        <div class="col">
-          <ul class="list-unstyled">
-            <li><a href="#">VISION</a></li>
-            <li><a href="#">ABOUT</a></li>
-            <li><a href="#">GET INVOLVED</a></li>
-          </ul>
+<div class="container-fluid pt-5" style="background-color:#15184D" >
+    <footer>  
+      <div class="pt-2 px-2 pb-3">
+        <div class="row row-cols-1 row-cols-md-5 text-white pb-5">
+          <div class="col">
+            <ul class="list-unstyled">
+              <li><a href="#" class="underable text-white">VISION</a></li>
+              <li><a href="#" class="underable text-white">ABOUT</a></li>
+              <li><a href="#" class="underable text-white">GET INVOLVED</a></li>
+            </ul>
+          </div>
+          <div class="col">
+            <ul class="list-unstyled">
+              <li><a href="#" class="underable text-white">X</a></li>
+              <li><a href="#" class="underable text-white">INSTAGRAM</a></li>
+              <li><a href="#" class="underable text-white">FACEBOOK</a></li>
+            </ul>
+          </div>
+          
         </div>
-        <div class="col">
-          <ul class="list-unstyled">
-            <li><a href="#">X</a></li>
-            <li><a href="#">INSTAGRAM</a></li>
-            <li><a href="#">FACEBOOK</a></li>
-          </ul>
-        </div>
-      </div>
-      <p class="text-white">&copy; 2024 by Mindful Mood</p>
-  </div>
-    
+        <hr />
+        <p class="text-white">&copy; 2024 by Mindful Mood</p>
+    </div>
+  </footer>
 </div>
