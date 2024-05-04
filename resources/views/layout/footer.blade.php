a1d511d1c598f0117a91a5a6562516dea1565549
GokaiInfinity <patriciofilbert@gmail.com>
2024-05-04 23:48:22 +0700

    Footer

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,5 +1,6 @@
+<<<<<<< Updated upstream
 <div class="container-fluid pt-5" style="background-color:#15184D" >
-    <footer>  
+    <footer>
       <div class="pt-2 px-2 pb-3">
         <div class="row row-cols-1 row-cols-md-3 text-white pb-5">
           <div class="col">
@@ -16,10 +17,36 @@
               <li><a href="#" class="underable text-white">FACEBOOK</a></li>
             </ul>
           </div>
-          
+
         </div>
         <hr />
         <p class="text-white">&copy; 2024 by Mindful Mood</p>
     </div>
   </footer>
 </div>
+=======
+<div class="container-fluid" style="background-color:#15184D" >
+    <footer>
+
+    <div class="container pt-2 pb-5">
+      <div class="row row-cols-1 row-cols-md-3 text-white pb-5">
+        <div class="col">
+          <ul class="list-unstyled">
+            <li><a href="#" class="text-white">VISION</a></li>
+            <li><a href="#" class="text-white">ABOUT</a></li>
+            <li><a href="#" class="text-white">GET INVOLVED</a></li>
+          </ul>
+        </div>
+        <div class="col">
+          <ul class="list-unstyled">
+            <li><a href="#" class="text-white">X</a></li>
+            <li><a href="#" class="text-white">INSTAGRAM</a></li>
+            <li><a href="#" class="text-white">FACEBOOK</a></li>
+          </ul>
+        </div>
+      </div>
+      <p class="text-white">&copy; 2024 by Mindful Mood</p>
+  </div>
+
+</div>
+>>>>>>> Stashed changes
