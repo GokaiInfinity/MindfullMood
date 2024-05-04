7995404554df3c9a0743dfd3db39fd38507fd5ac
GokaiInfinity <patriciofilbert@gmail.com>
2024-05-04 23:52:42 +0700

    Footer Update

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 <div class="container-fluid pt-5" style="background-color:#15184D" >
     <footer>
       <div class="pt-2 px-2 pb-3">
@@ -24,29 +23,3 @@
     </div>
   </footer>
 </div>
-=======
-<div class="container-fluid" style="background-color:#15184D" >
-    <footer>
-
-    <div class="container pt-2 pb-5">
-      <div class="row row-cols-1 row-cols-md-3 text-white pb-5">
-        <div class="col">
-          <ul class="list-unstyled">
-            <li><a href="#" class="text-white">VISION</a></li>
-            <li><a href="#" class="text-white">ABOUT</a></li>
-            <li><a href="#" class="text-white">GET INVOLVED</a></li>
-          </ul>
-        </div>
-        <div class="col">
-          <ul class="list-unstyled">
-            <li><a href="#" class="text-white">X</a></li>
-            <li><a href="#" class="text-white">INSTAGRAM</a></li>
-            <li><a href="#" class="text-white">FACEBOOK</a></li>
-          </ul>
-        </div>
-      </div>
-      <p class="text-white">&copy; 2024 by Mindful Mood</p>
-  </div>
-
-</div>
->>>>>>> Stashed changes
