f6cbd87e53fe4acacd369cf79169b23d0a047be8
GokaiInfinity <patriciofilbert@gmail.com>
2024-05-05 01:58:21 +0700

    donatescript

--- resources/views/HomeComponents/part3.blade.php
+++ resources/views/HomeComponents/part3.blade.php
@@ -1,6 +1,6 @@
 <div class="stickysec">
     <div class="container-fluid p-5 part3 d-flex justify-content-center bg-white">
-        <div class="motto mx-auto my-auto fw-bold font-mono text-center">
+        <div class="motto mx-auto my-auto fw-bold font-mono text-center" id="motto">
             PRIORITIZING MENTAL HEALTH FOR A HEALTHTIER YOU
         </div>
     </div>
--- resources/views/donate.blade.php
+++ resources/views/donate.blade.php
@@ -1,6 +1,6 @@
 @extends('layout.app')
 
 @section('content')
-@vite('resources/css/donate.css')
+@vite(['resources/css/donate.css', 'resources/js/donatescript.js'])
 @include('DonateComponents.body')
 @endsection
