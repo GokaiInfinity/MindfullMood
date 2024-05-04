9fcae6c0924a19bf324b4dacb576451fa0c7103a
JDTOmoi <jasondavetahir@gmail.com>
2024-05-04 13:42:42 +0700

    added images and readjusted font weights and borders

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -1,34 +1,38 @@
 @extends('layout.app')
 
 @section('content')
+@vite('resources/css/app.css')
 <div class="col px-5 py-3">
     <div class="row mx-auto py-4">
         <div class="col-sm-4">
             <div class="card border-0 shadow" style="border-radius: 0;">
+                <img src="{{ asset('storage/work together.webp') }}" alt="Image" class="img-fluid">
                 <div class="card-body px-4 py-5">
-                    <h4 class="card-title">Advantages of having romantic relationship in Colleges</h4>
-                    <h6 class="card-subtitle">1 Mar 2023</h6><br/>
-                    <p class="card-text">Compilation of advice from mentors & experts around the world, 18 country partner has joined MindfullMood like United States, German, France, China, Japan, Etc</p>
+                    <h4 class="card-title fw-normal">Advantages of having romantic relationship in Colleges</h4>
+                    <h6 class="card-subtitle newscard">1 Mar 2023</h6><br/>
+                    <p class="card-text newscard pb-3">Compilation of advice from mentors & experts around the world, 18 country partner has joined MindfullMood like United States, German, France, China, Japan, Etc</p>
                     <a href="#" class="sqbtn">Read More</a>
                 </div>
             </div>
         </div>
         <div class="col-sm-4">
             <div class="card border-0 shadow" style="border-radius: 0;">
+                <img src="{{ asset('storage/Asian indie singer performing.webp') }}" alt="Image" class="img-fluid">
                 <div class="card-body px-4 py-5">
-                    <h4 class="card-title">Various Benefits of Singing or Karaoke for Psychological Health</h4>
-                    <h6 class="card-subtitle">1 Mar 2023</h6><br/>
-                    <p class="card-text">Compilation of advice from mentors & experts around the world, 18 country partner has joined MindfullMood like United States, German, France, China, Japan, Etc</p>
+                    <h4 class="card-title fw-normal">Various Benefits of Singing or Karaoke for Psychological Health</h4>
+                    <h6 class="card-subtitle newscard">1 Mar 2023</h6><br/>
+                    <p class="card-text newscard pb-3">Compilation of advice from mentors & experts around the world, 18 country partner has joined MindfullMood like United States, German, France, China, Japan, Etc</p>
                     <a href="#" class="sqbtn">Read More</a>
                 </div>
             </div>
         </div>
         <div class="col-sm-4">
             <div class="card border-0 shadow" style="border-radius: 0;">
-                <div class="card-body px-4 py-5">
-                    <h4 class="card-title">Various Benefits of Singing or Karaoke for Psychological Health</h4>
-                    <h6 class="card-subtitle">1 Mar 2023</h6><br/>
-                    <p class="card-text">Compilation of advice from mentors & experts around the world, 18 country partner has joined MindfullMood like United States, German, France, China, Japan, Etc</p>
+            <img src="{{ asset('storage/playing games.webp') }}" alt="Image" class="img-fluid">
+                <div class="card-body px-4 py-5">   
+                    <h4 class="card-title fw-normal">Some advantages of playing games and ideal proportions of time</h4>
+                    <h6 class="card-subtitle newscard">1 Mar 2023</h6><br/>
+                    <p class="card-text newscard pb-3">Compilation of advice from mentors & experts around the world, 18 country partner has joined MindfullMood like United States, German, France, China, Japan, Etc</p>
                     <a href="#" class="sqbtn">Read More</a>
                 </div>
             </div>
@@ -37,30 +41,33 @@
     <div class="row mx-auto py-4">
         <div class="col-sm-4">
             <div class="card border-0 shadow" style="border-radius: 0;">
+            <img src="{{ asset('storage/3 girls smiles.webp') }}" alt="Image" class="img-fluid">
                 <div class="card-body px-4 py-5">
-                    <h4 class="card-title">Advantages of having romantic relationship in Colleges</h4>
-                    <h6 class="card-subtitle">1 Mar 2023</h6><br/>
-                    <p class="card-text">Compilation of advice from mentors & experts around the world, 18 country partner has joined MindfullMood like United States, German, France, China, Japan, Etc</p>
+                    <h4 class="card-title fw-normal">Advantages of having romantic relationship in Colleges</h4>
+                    <h6 class="card-subtitle newscard">1 Mar 2023</h6><br/>
+                    <p class="card-text newscard pb-3">Compilation of advice from mentors & experts around the world, 18 country partner has joined MindfullMood like United States, German, France, China, Japan, Etc</p>
                     <a href="#" class="sqbtn">Read More</a>
                 </div>
             </div>
         </div>
         <div class="col-sm-4">
             <div class="card border-0 shadow" style="border-radius: 0;">
+            <img src="{{ asset('storage/Yoga.webp') }}" alt="Image" class="img-fluid">
                 <div class="card-body px-4 py-5">
-                    <h4 class="card-title">Various Benefits of Singing or Karaoke for Psychological Health</h4>
-                    <h6 class="card-subtitle">1 Mar 2023</h6><br/>
-                    <p class="card-text">Compilation of advice from mentors & experts around the world, 18 country partner has joined MindfullMood like United States, German, France, China, Japan, Etc</p>
+                    <h4 class="card-title fw-normal">The Importance of Yoga to Release Stress and Empty the Mind</h4>
+                    <h6 class="card-subtitle newscard">1 Mar 2023</h6><br/>
+                    <p class="card-text newscard pb-3">Compilation of advice from mentors & experts around the world, 18 country partner has joined MindfullMood like United States, German, France, China, Japan, Etc</p>
                     <a href="#" class="sqbtn">Read More</a>
                 </div>
             </div>
         </div>
         <div class="col-sm-4">
             <div class="card border-0 shadow" style="border-radius: 0;">
+            <img src="{{ asset('storage/work together (1).webp') }}" alt="Image" class="img-fluid">
                 <div class="card-body px-4 py-5">
-                    <h4 class="card-title">Various Benefits of Singing or Karaoke for Psychological Health</h4>
-                    <h6 class="card-subtitle">1 Mar 2023</h6><br/>
-                    <p class="card-text">Compilation of advice from mentors & experts around the world, 18 country partner has joined MindfullMood like United States, German, France, China, Japan, Etc</p>
+                    <h4 class="card-title fw-normal">How to Work Under Pressure Without Mental Fatigue</h4>
+                    <h6 class="card-subtitle newscard">1 Mar 2023</h6><br/>
+                    <p class="card-text newscard pb-3">Compilation of advice from mentors & experts around the world, 18 country partner has joined MindfullMood like United States, German, France, China, Japan, Etc</p>
                     <a href="#" class="sqbtn">Read More</a>
                 </div>
             </div>
