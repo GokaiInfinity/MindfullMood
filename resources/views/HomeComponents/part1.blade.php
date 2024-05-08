5f94e1cb6147fd6b8c0584d9bf565ebaf41a64d0
GokaiInfinity <patriciofilbert@gmail.com>
2024-05-08 12:18:17 +0700

    Part1 Text

--- resources/views/HomeComponents/part1.blade.php
+++ resources/views/HomeComponents/part1.blade.php
@@ -2,10 +2,8 @@
     <div class = "row">
         <div class = "col-md-6 mt-5 font-serif fs-5 lh-sm d-grid gap-5 part1_1">
             <div class="mb-5">
-                At Mindful Mood, we believe that everyone deserves access to quality mental healthcare. That is why we
-                are proud to offer our services to people all over the world. Our team consists of professionals from
-                diverse backgrounds, who are all dedicated to using their skills and knowledge to help people who are
-                struggling with their mental health. We provide counseling and therapy, as well as a range of other
+                At Mindful Mood, we believe that everyone deserves access to quality mental healthcare. We provide
+                counseling and therapy, as well as a range of other
                 services that are designed to meet the needs of our clients. Whether you are looking for support for
                 yourself or a loved one, we are here to help.
             </div>
@@ -16,8 +14,7 @@
         <div class= "col-md-1">
         </div>
         <div class= "col-md-5">
-        @include('HomeComponents.subscribecard')
+            @include('HomeComponents.subscribecard')
         </div>
     </div>
 </div>
-
