c50b7e3604b2c22268dceec391fdb620c9ab59b8
GokaiInfinity <patriciofilbert@gmail.com>
2024-05-15 02:18:37 +0700

    Card Unselect

--- resources/js/app.js
+++ resources/js/app.js
@@ -20,6 +20,8 @@ function applySlideInAnimation() {
 // Attach the function to the window.onload event
 window.onload = applySlideInAnimation;
 
+
+//main page donation
 document.addEventListener('DOMContentLoaded', function() {
     // Fetch all elements with the class 'donatecard'
     const donateCards = document.querySelectorAll('.donatecard');
@@ -27,13 +29,18 @@ document.addEventListener('DOMContentLoaded', function() {
     // Add click event listener to each card
     donateCards.forEach(function(card) {
         card.addEventListener('click', function() {
-            // Remove 'selected' class from all cards
-            donateCards.forEach(function(c) {
-                c.classList.remove('selected','no-hover');
-            });
+            // If the card is already selected, remove the 'selected' and 'no-hover' classes
+            if (card.classList.contains('selected') && card.classList.contains('no-hover')) {
+                card.classList.remove('selected', 'no-hover');
+            } else {
+                // Remove 'selected' and 'no-hover' classes from all cards
+                donateCards.forEach(function(c) {
+                    c.classList.remove('selected', 'no-hover');
+                });
 
-            // Add 'selected' class to the clicked card
-            card.classList.add('selected', 'no-hover');
+                // Add 'selected' and 'no-hover' classes to the clicked card
+                card.classList.add('selected', 'no-hover');
+            }
         });
     });
 });
