c7ce5705e735d1e839bf03ba1f60af02f8481da6
JDTOmoi <jasondavetahir@gmail.com>
2024-05-27 20:29:47 +0700

    made fonts work

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -11,6 +11,11 @@
     <title>MindfullMood</title>
 
     <link rel="stylesheet" href="css/app.css">
+
+    <!-- Scripts -->
+    @yield('headscripts')
+    @vite(['resources/sass/app.scss', 'resources/js/app.js', 'resources/css/headerfooter.css'])
+
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -18,9 +23,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
-    <!-- Scripts -->
-    @yield('headscripts')
-    @vite(['resources/sass/app.scss', 'resources/js/app.js', 'resources/css/headerfooter.css'])
+
 </head>
 
 <body>
